826aa574132d35407dcb45f989a0cc79e56d5abe
adel mensi <adelmensi8@gmail.com>
2026-05-30 17:21:42 +0100

    Initial commit: XOR Neural Network from scratch

---
+++ XOR.py
@@ -0,0 +1,71 @@
+import math
+import numpy as np
+def sigmoid(z):
+    e = math.exp(-z)
+    return 1/(1+e)
+
+
+x_train = np.array([
+    [1, 0, 0],
+    [1, 0, 1],
+    [1, 1, 0],  
+    [1, 1, 1] 
+])
+y_train = np.array([0, 1, 1, 0])
+
+np.random.seed(10)
+
+neuron_1 = np.random.uniform(-1, 1, 3)
+neuron_2 = np.random.uniform(-1, 1, 3)
+neuron_3 = np.random.uniform(-1, 1, 3)
+
+lear_rate = 0.5
+neurons=[neuron_1,neuron_2,neuron_3]
+
+lear_rate=0.7
+
+neuron_output=np.array([0.0,0.0,0.0])
+
+def forward(i):
+    neuron_output[0]=sigmoid(np.dot(x_train[i],neuron_1))
+    neuron_output[1]=sigmoid(np.dot(x_train[i],neuron_2))
+    x=np.dot(neuron_3[1:3],neuron_output[0:2])
+    neuron_output[2]=sigmoid(neuron_3[0]+x)
+
+
+def backward(y,i):
+    global neuron_1, neuron_2, neuron_3
+    #neuron 3
+    der_cost=neuron_output[2]-y
+    delta = der_cost*neuron_output[2]*(1-neuron_output[2])
+    w31 = delta*1
+    w32 = delta*neuron_output[0]
+    w33 = delta*neuron_output[1]
+    #neuron 2
+    delta1 = delta*neuron_3[2]*neuron_output[1]*(1-neuron_output[1])
+    w21 = delta1*x_train[i][0]
+    w22 = delta1*x_train[i][1]
+    w23 = delta1*x_train[i][2]
+    #neuron 3
+    delta2 = delta*neuron_3[1]*neuron_output[0]*(1-neuron_output[0])
+    w11 = delta2*x_train[i][0]
+    w12 = delta2*x_train[i][1]
+    w13 = delta2*x_train[i][2]
+
+    #update
+
+    neuron_3 -= lear_rate * np.array([w31, w32, w33])
+    neuron_2 -= lear_rate * np.array([w21, w22, w23])
+    neuron_1 -= lear_rate * np.array([w11, w12, w13])
+
+for epoch in range(10000):
+    for i in range(len(x_train)):
+        forward(i)
+        backward(y_train[i], i)
+
+for i in range(len(x_train)):
+    forward(i)
+    print(f"Entrées: {x_train[i][1:]} | Cible: {y_train[i]} | Prédiction: {neuron_output[2]:.4f}")
+
+
+
